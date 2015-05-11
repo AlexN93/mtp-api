4cf5a84e9ad7da0a0f438e5e04bcc0926ae789bf
AlexN <aleksandar.m.nikolov@gmail.com>
2015-05-11 21:27:59 +0300

    country field added to index page

--- src/LexxTech/MTPBundle/Resources/public/js/app.js
+++ src/LexxTech/MTPBundle/Resources/public/js/app.js
@@ -3,7 +3,7 @@ $(document).ready(function(){
         $.ajax({  
             type: "POST",  
             url: "/registerTransaction",  
-            data: '{"userId": "134256", "currencyFrom": "EUR", "currencyTo": "GBP", "amountSell": 1000, "amountBuy": 747.10, "rate": 0.7471, "timePlaced" : "24-JAN-15 10:27:44", "originatingCountry" : "FR"}',
+            data: '{"userId": "134256", "currencyFrom": "EUR", "currencyTo": "GBP", "amountSell": 1000, "amountBuy": 747.10, "rate": 0.7471, "timePlaced" : "24-JAN-15 10:27:44", "originatingCountry" : "' + $("#select-country").val() + '"}',
             success: function(response) {
                 console.log(response);              
             },
